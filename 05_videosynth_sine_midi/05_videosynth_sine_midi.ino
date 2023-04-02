8732c03fcc547a73a44d6b797d24f81bb4e2b7f7
Tod Kurt <todbot@users.noreply.github.com>
2023-04-02 13:24:35 -0700

    05 cleanup

--- 05_videosynth_sine_midi/05_videosynth_sine_midi.ino
+++ 05_videosynth_sine_midi/05_videosynth_sine_midi.ino
@@ -2,6 +2,7 @@
 // 31 Mar 2023 - @todbot / Tod Kurt
 //
 // Must set:  Tools / USB Stack -> "Adafruit TinyUSB"
+// 
 
 #include <PicoDVI.h> 
 
@@ -10,20 +11,25 @@
 
 DVIGFX16 display(DVI_RES_320x240p60, adafruit_feather_dvi_cfg);
 
-const int midi_tx_pin = 0;
-const int midi_rx_pin = 1;
-
 Adafruit_USBD_MIDI usb_midi;  // USB MIDI object
 MIDI_CREATE_INSTANCE(Adafruit_USBD_MIDI, usb_midi, MIDIusb); // USB MIDI
-//MIDI_CREATE_INSTANCE(HardwareSerial, Serial1, MIDIserial);   // Serial MIDI
+MIDI_CREATE_INSTANCE(HardwareSerial, Serial1, MIDIserial);   // Serial MIDI
+
+int fade_amount = 8; // how fast previous frame fades away, higher = faster fade
+const uint32_t millis_until_attract_mode = 10 * 1000;
+
+const int midi_tx_pin = 0;  // MIDI Feather Wing
+const int midi_rx_pin = 1;  
 
 int16_t dw,dh;
-uint32_t last_millis = 0;
+uint32_t last_millis;
 uint32_t last_millis_hue;
 uint32_t last_millis_midi;
 
-float noise_max = 10;
-int fade_amount = 8; // how fast previous frame fades away, higher = faster fade
+// for note-stealing algorithm in noteOn()
+const uint8_t notes_cnt = 6; //shapes_cnt; // FIXME: match shapes_cnt
+uint8_t notes_on[notes_cnt];
+uint32_t notes_millis[notes_cnt];
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
@@ -36,42 +42,70 @@ void setup() {
 
   shapes_init();
   
-//  Serial1.setRX(midi_rx_pin);
-//  Serial1.setTX(midi_tx_pin);
+  Serial1.setRX(midi_rx_pin);
+  Serial1.setTX(midi_tx_pin);
 
   MIDIusb.begin(MIDI_CHANNEL_OMNI);
-//  MIDIserial.begin(MIDI_CHANNEL_OMNI);
+  MIDIserial.begin(MIDI_CHANNEL_OMNI);
 
   MIDIusb.turnThruOff();    // turn off echo
-//  MIDIserial.turnThruOff(); // turn off echo
+  MIDIserial.turnThruOff(); // turn off echo
 
 }
 
 void loop() { 
   handleMIDIusb();
-//  handleMIDIserial();
+  handleMIDIserial();
   do_video();
 }
 
-const uint8_t note_cnt = 3; // FIXME: match shape_cnt
-uint8_t notes_on[note_cnt];
-uint8_t curr_note = 0;
 //
 void handleNoteOn(byte channel, byte note, byte vel) {
   Serial.printf("handleNoteOn! note:%d vel:%d\n", note, vel);
   digitalWrite(LED_BUILTIN,HIGH);
+  // cancel attract mode
+  if( millis() - last_millis_midi > millis_until_attract_mode ) { 
+    for(int i=0; i<notes_cnt; i++) { 
+      shapes_print(i); // debug
+      shapes_shape_off(i, 0, 0, 0.5);
+     }
+  }  
   last_millis_midi = millis();
-  curr_note = (curr_note + 1) % note_cnt;
-  notes_on[curr_note] = note;
-  shapes_shape_on(curr_note, note, vel);
+    
+  // note stealing algorithm
+  uint32_t oldest_note_millis = 0;
+  int oldest_note = -1;  
+  for( int i=0; i< notes_cnt; i++) { 
+    if( notes_millis[i] == 0 ) {  // a free slot
+      notes_on[i] = note;
+      notes_millis[i] = millis();
+      shapes_shape_on(i, note, vel, 1.001);
+      oldest_note = -1;
+      break;
+    }
+    else if( notes_millis[i] > oldest_note_millis ) { 
+      oldest_note_millis = notes_millis[i];
+      oldest_note = i;
+    }
+  }
+  if( oldest_note != -1 ) {
+    Serial.printf("using oldest note: %d\n", oldest_note);
+    notes_on[oldest_note] = note;
+    notes_millis[oldest_note] = millis();
+    shapes_shape_on(oldest_note, note, vel, 1.001); // slow increase in size
+  }
+  
 }
+
 //
 void handleNoteOff(byte channel, byte note, byte vel) {
   Serial.printf("handleNoteOff! note:%d vel:%d\n", note, vel);
   digitalWrite(LED_BUILTIN,LOW);
-  for( int i=0; i<note_cnt; i++) { 
+  last_millis_midi = millis();
+  for( int i=0; i<notes_cnt; i++) { 
     if( notes_on[i] == note ) { 
-      shapes_shape_off(i, note, vel);      
+      shapes_shape_off(i, note, vel, 0.99); // faster decrease in size
+      notes_millis[i] = 0; // free up note slot
     }
   }
 }
@@ -79,7 +113,9 @@ void handleNoteOff(byte channel, byte note, byte vel) {
 //
 void do_video() {
   // fade last frame
-  if( millis() - last_millis > 30 ) { 
+  // why 30 millis? seemed like a good idea,
+  // many other magic variables sort of depend on this though
+  if( millis() - last_millis > 30 ) {
     last_millis = millis();
     dim_buf16(&display, fade_amount); // fade last frame
     shapes_update();
@@ -88,11 +124,50 @@ void do_video() {
   // attract mode and auto hue shift
   if( millis() - last_millis_hue > 100 ) { 
     last_millis_hue = millis();
-    if( millis() - last_millis_midi > 5000 ) { // attract mode if no MIDI
+    // attract mode if no MIDI
+    if( millis() - last_millis_midi > millis_until_attract_mode ) { 
       shapes_attract();
     }
   }
 
   shapes_draw(&display);
   
+}
+
+//
+void handleMIDIusb() {
+  while( MIDIusb.read() ) {  // use while() to read all pending MIDI, shouldn't hang
+    switch(MIDIusb.getType()) {
+      case midi::ControlChange:
+        //handleControlChange(0, MIDIusb.getData1(), MIDIusb.getData2());
+        break;
+      case midi::NoteOn:
+        handleNoteOn( 0, MIDIusb.getData1(),MIDIusb.getData2());
+        break;
+      case midi::NoteOff:
+        handleNoteOff( 0, MIDIusb.getData1(),MIDIusb.getData2());
+        break;
+      default:
+        break;
+    }
+  }
+}
+
+// surely there's a way to have this generalized
+void handleMIDIserial() {
+  while( MIDIserial.read() ) {  // use while() to read all pending MIDI, shouldn't hang
+    switch(MIDIserial.getType()) {
+      case midi::ControlChange:
+        //handleControlChange(0, MIDIserial.getData1(), MIDIserial.getData2());
+        break;
+      case midi::NoteOn:
+        handleNoteOn( 0, MIDIserial.getData1(),MIDIserial.getData2());
+        break;
+      case midi::NoteOff:
+        handleNoteOff( 0, MIDIserial.getData1(),MIDIserial.getData2());
+        break;
+      default:
+        break;
+    }
+  }
 }
